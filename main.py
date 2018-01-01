5526543fd3c4714b59fd36adb138bb80ca533092
Elia P <open.e@patzschke.net>
2018-01-01 04:00:30 +0100

    - Add Addition - Add Mainloop - Add v.1 Chooser

---
+++ main.py
@@ -0,0 +1,22 @@
+# Little Command-Line Calc
+# Written by: ENic2004
+
+#Import 2nd File
+from operations import ops
+
+#Input Manager(inmgr)
+class inmgr:
+    def main():
+        print("Here the wlctext has to go!")
+        f = int(input("Wich Function to do? "))
+    def choose(f):
+        if f = 1:
+            ops.add(x, y)
+            inmgr.main()
+        else:
+            print("Not known Function!")
+            inmgr.main()
+
+#Start MainLoop
+#------ DO NOT PUT CODE BELOW, WILL BE NEVER PROCEED -------
+inmgr.main()
---
+++ operations.py
@@ -0,0 +1,6 @@
+from math import *
+
+class ops():
+    def add(a, b):
+        c = a + b
+        print(c)
