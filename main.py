7013b575836299a688625a5e09a11c29f533b3fb
Elia P <open.e@patzschke.net>
2018-01-01 04:29:20 +0100

    - Fixed Bugs - Add v1.1 InputMgr

--- main.py
+++ main.py
@@ -4,19 +4,29 @@
 #Import 2nd File
 from operations import ops
 
-#Input Manager(inmgr)
-class inmgr:
+#Input Manager(cl)
+class cl:
     def main():
         print("Here the wlctext has to go!")
         f = int(input("Wich Function to do? "))
+        cl.choose(f)
     def choose(f):
-        if f = 1:
+        if f == 1:
+            cl.inputmgr()
             ops.add(x, y)
-            inmgr.main()
+            cl.main()
+        if f == '':
+            print("Not known Function!")
+            cl.main()
         else:
             print("Not known Function!")
-            inmgr.main()
+            cl.main()
+    def inputmgr():
+        global x
+        global y
+        x = int(input("First Number: "))
+        y = int(input("Second Number: "))
 
 #Start MainLoop
 #------ DO NOT PUT CODE BELOW, WILL BE NEVER PROCEED -------
-inmgr.main()
+cl.main()
